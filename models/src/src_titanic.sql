5a079acbde29733bb95c50cf4dbc90cf92d89a4e
Mahmoud Farah <mfarah@bitbang.com>
2025-06-11 15:13:24 +0000

    test2

--- models/src/src_titanic.sql
+++ models/src/src_titanic.sql
@@ -1,15 +1,7 @@
 -- import raw_listings
-WITH raw_listings AS (
-        SELECT * FROM {{ source('raw', 'listings') }}
+WITH raw_titanic AS (
+        SELECT * FROM {{ source('raw', 'titanic') }}
 )
 SELECT 
-   id AS listing_id,
-   listing_url,
-   name AS listing_name,
-   room_type,
-   minimum_nights,
-   host_id,
-   price AS price_str,
-   created_at,
-   updated_at
-FROM raw_listings
+   *
+FROM raw_titanic
